f3898e6178967066a16568da452ee96987279023
kalkin <kalkin@8a1b890e-4721-0410-9921-f27540cc7118>
2006-11-05 17:25:56 +0000

    first commit :)

---
+++ trunk/TestBot.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package de.blase16.jajbof;
+
+import org.jivesoftware.smack.XMPPException;
+
+/**
+ * @author blase16
+ *
+ */
+public class TestBot extends JAJBot {
+
+ 
+    /**
+     * @param jid
+     * @param pw
+     * @throws XMPPException
+     */
+    public TestBot(String jid, String pw) throws XMPPException {
+	super(jid, pw);
+	// TODO Auto-generated constructor stub
+    }
+
+    public String unknownMessage(String msg, String user) {
+	String result = "";
+	if(msg.startsWith("suche:")){
+	    int i = Integer.parseInt((msg.split("suche:")[1].trim()));
+	    return "" + i++;
+	}
+	if(msg.equals("next")){
+	    History history = map.get(user);
+	    int j = history.lastIndexOf("suche:1");
+	    
+	    return "" + (history.size() - j);
+	    
+	}
+	
+	
+	
+	
+	return result;
+    }
+}
